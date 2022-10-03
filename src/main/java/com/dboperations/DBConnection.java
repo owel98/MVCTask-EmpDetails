a1b58c970fcee3d1dda5fb8cf760ce629a51ccff
owel98 <113821086+owel98@users.noreply.github.com>
2022-10-03 12:50:22 +0530

    Update DBConnection.java

--- src/main/java/com/dboperations/DBConnection.java
+++ src/main/java/com/dboperations/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
 	public static Connection getConnect() throws SQLException {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connect=DriverManager.getConnection("jdbc:mysql://localhost:3306/spring","root","satya@704771");
+			connect=DriverManager.getConnection("jdbc:mysql://localhost:3306/javahibernate","root","$rinivas&799@vA$");
 			return connect;
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
